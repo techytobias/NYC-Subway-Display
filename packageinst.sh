86d7bdc7e7d22731d524806d987b378337957314
techytobias <tobiasheller1@gmail.com>
2022-05-16 12:19:24 -0400

    Added Files

---
+++ packageinst.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo pip3 install --upgrade google-api-python-client -t /home/pi/rpi-rgb-led-matrix/bindings/python
+sudo pip3 install pandas -t /home/pi/rpi-rgb-led-matrix/bindings/python
+sudo pip3 install google-cloud -t /home/pi/rpi-rgb-led-matrix/bindings/python
+sudo pip3 install google-cloud-vision -t /home/pi/rpi-rgb-led-matrix/bindings/python
+sudo pip3 install --upgrade gtfs-realtime-bindings -t /home/pi/rpi-rgb-led-matrix/bindings/python
+sudo pip3 install python-dotenv -t /home/pi/rpi-rgb-led-matrix/bindings/python
+sudo pip3 install protobuf3-to-dict -t /home/pi/rpi-rgb-led-matrix/bindings/python
+sudo pip3 install --user gtfs-realtime-bindings -t /home/pi/rpi-rgb-led-matrix/bindings/python
+
