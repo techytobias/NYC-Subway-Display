d524c32f4590bcfe972529ce3938c20f115f0b8e
techytobias <tobiasheller1@gmail.com>
2023-03-07 10:26:03 -0500

    V2 Update!
    New Disruptions page that shows lines with unplanned active issues, text for long destinations now scrolls.
    
    Also, use of pandas has been eliminated with new stops.csv

--- mtacalls2.py
+++ mtacalls2.py
@@ -4,13 +4,14 @@ import requests
 import time # imports module for Epoch/GMT time conversion
 import os # imports package for dotenv
 from protobuf_to_dict import protobuf_to_dict
+import subprocess
 
 def getdata():
     realtime_data1=[]
     links=["https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace","https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm", "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs"]
     for link in links:
         feed = gtfs_realtime_pb2.FeedMessage()
-        response = requests.get(link, headers={"x-api-key": 'API KEY HERE KEEP APOSTROPHES'})
+        response = requests.get(link, headers={"x-api-key": '5HjyreyVOg7bKhtCMLfqd1OYuMMbnWyT5zc2lUFN'})
         feed.ParseFromString(response.content)
         subway_feed = protobuf_to_dict(feed) # subway_feed is a dictionary
         realtime_data = subway_feed['entity'] # train_data is a list
@@ -31,27 +32,36 @@ def station_time_lookup(train_data, station):
     ctimes=[]
     for trains in train_data: # trains are dictionaries
         if trains.get('trip_update', False) != False:
-            unique_train_schedule = trains['trip_update'] # train_schedule is a dictionary with trip and stop_time_update
+            unique_train_schedule = trains['trip_update'] # train_schedule is a dictionary with trip 
             try:
                 unique_arrival_times = unique_train_schedule['stop_time_update'] # arrival_times is a list of arrivals
                 for scheduled_arrivals in unique_arrival_times: #arrivals are dictionaries with time data and stop_ids
                     if scheduled_arrivals.get('stop_id', False) == station:
                         s1=unique_train_schedule
                         trainletter=s1['trip']['route_id']
-                        laststop=unique_arrival_times[-1]['stop_id']
+
+                        test5=unique_arrival_times[-1]
+                        stop=test5['stop_id']
+
                         time_data = scheduled_arrivals['arrival']
                         unique_time = time_data['time']
+                        
                         if unique_time != None:
-                            mintoarrival=int(((unique_time - int(time.time())) / 60))
+                            mintoarrival=mintoarrival=int(((unique_time - int(time.time())) / 60))
                             if mintoarrival > 2:
-                                ctimes.append([trainletter, mintoarrival, laststop])
+                                ctimes.append([trainletter, mintoarrival, stop])
             except:
                 pass
     return ctimes
 
 def totalstationtimes(stationlist):
     finaldata=[]
-    data=getdata()
+    try:
+        data=getdata()
+    except:
+        print("datafail")
+        time.sleep(30)
+        subprocess.Popen('sudo reboot -n', shell=True)
     print("datagot")
     for station in stationlist:
         stations=[(station + "N"), (station + "S")]
@@ -64,4 +74,39 @@ def totalstationtimes(stationlist):
         finaldata.append(newdata)
     return finaldata
 
-print(totalstationtimes(["F06", "D43"]))
+def getservicedata():
+    realtime_data1=[]
+    links=["https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts"]
+    for link in links:
+        feed = gtfs_realtime_pb2.FeedMessage()
+        response = requests.get(link, headers={"x-api-key": 'YOUR-API-KEY-KEEP-THE-APOSTROPHES'})
+        feed.ParseFromString(response.content)
+        subway_feed = protobuf_to_dict(feed) # subway_feed is a dictionary
+        realtime_data = subway_feed['entity'] # train_data is a list
+        for element in realtime_data:
+            realtime_data1.append(element)
+    return realtime_data1
+
+
+def procservicedata():
+    problemtrains=[]
+    mainlist=getservicedata()[:10]
+    mainlist2=[]
+    for element in mainlist:
+        if element["id"][4]=="a":
+            mainlist2.append(element["alert"]["informed_entity"])
+
+    for element4 in mainlist2:
+        for element5 in element4:
+            try:
+                if element5["route_id"] not in problemtrains:
+                    if "S" not in element5["route_id"]:
+                        problemtrains.append(element5["route_id"])
+            except:
+                pass
+    if len(problemtrains)==0:
+        problemtrains=["None"]
+    problemtrains.sort()
+    return problemtrains
+#print(totalstationtimes(["232", "A41"]))
+print(procservicedata())
--- rundisplay.py
+++ rundisplay.py
@@ -1,95 +1,188 @@
+#!/usr/bin/env python
 from samplebase import SampleBase
 from rgbmatrix import graphics
-from pandas import pandas as pd
+from subprocess import check_output
+import csv
 import time
 import mtacalls2
-
-df=pd.read_csv("stops.csv")
-df.head()
-stops=pd.Series(df.stop_name.values,index=df.stop_id).to_dict()
+import os
+stops = {}
+with open("stops.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    for row in csvreader:
+        stops.update({row[0]: row[1]})
 
 class GraphicsTest(SampleBase):
-    def __init__(self, packet, *args, **kwargs):
+    def __init__(self, packet, servicedata, *args, **kwargs):
         super(GraphicsTest, self).__init__(*args, **kwargs)
         self.packet=packet
-
-    def run(self):
-        canvas = self.matrix
-        font = graphics.Font()
-        font.LoadFont("../../../fonts/5x8.bdf")
+        self.servicedata=servicedata
+    def getcolor(self, trainline):
         white=graphics.Color(255, 255, 255)
         seventh = graphics.Color(255, 0, 0)
         lexington = graphics.Color(0, 146, 66)
         eigth = graphics.Color(0, 57, 163)
         sixth = graphics.Color(255, 99, 32)
+        bway=graphics.Color(255,255,0)
+        jamaica=graphics.Color(165,42,42)
+        nassau=graphics.Color(107,187,78)
+        flushing=graphics.Color(185,52,170)
+        atlantic=graphics.Color(167,169,172)
+        if trainline=="4" or trainline=="5" or trainline=="6":
+            color=lexington
+        elif trainline=="3" or trainline=="2" or trainline=="1":
+            color=seventh
+        elif trainline=="J" or trainline=="Z":
+            color=jamaica
+        elif trainline=="A" or trainline=="C" or trainline=="E" or trainline=="H":
+            color=eigth
+        elif trainline=="F" or trainline=="M" or trainline=="B" or trainline=="D":
+            color=sixth
+        elif trainline=="N" or trainline=="Q" or trainline=="R" or trainline=="W":
+            color=bway
+        elif trainline=="G":
+            color=nassau
+        elif trainline=="L":
+            color=atlantic
+        elif trainline=="7":
+            color=flushing
+        else:
+            color=white
+        return color
+    def run(self):
+        canvas = self.matrix.CreateFrameCanvas()
+        font = graphics.Font()
+        font.LoadFont("../../../fonts/5x8.bdf")
         black= graphics.Color(0, 0, 0)
-      
-        for i in range(63):
-            graphics.DrawLine(canvas, 0, i, 64, i, black)
+        white=graphics.Color(255, 255, 255)
+        canvas.Clear()
         c=0
         for subpacket in self.packet:
             c+=1
-            for i in range(63):
-                graphics.DrawLine(canvas, 0, i, 64, i, black)
-            
-            traincharspacing=7
-            b=0
-            for train in subpacket:
-                if train[2][3] == "N":
-                    if b<2:
-                        if train[0]=="4" or train[0]=="5":
-                            color=lexington
-                        elif train[0]=="3" or train[0]=="2":
-                            color=seventh
-                        elif train[0]=="A" or train[0]=="C":
-                            color=eigth
-                        elif train[0]=="F" or train[0]=="M":
-                            color=sixth
-                        else:
-                            color=white
-                        try:
-                            textstring=str(train[0]+" "+str(stops[train[2]][:8]))
-                        except:
-                            textstring="textfail"
-                        graphics.DrawText(canvas, font, 0, traincharspacing, color, textstring)
-                        graphics.DrawText(canvas, font, 53, traincharspacing, color, str(train[1]))
-                        traincharspacing+=8
-                        b+=1
-            traincharspacing=23
-            b=0
-            for train in subpacket:
-                if train[2][3] == "S":
-                    if b<2:
-                        if train[0]=="4" or train[0]=="5":
-                            color=lexington
-                        elif train[0]=="3" or train[0]=="2":
-                            color=seventh
-                        elif train[0]=="A" or train[0]=="C":
-                            color=eigth
-                        elif train[0]=="F" or train[0]=="M":
-                            color=sixth
-                        else:
-                            color=white
-                        try:
-                            textstring=str(train[0]+" "+str(stops[train[2]][:8]))
-                        except:
-                            textstring="textfail"
-                        graphics.DrawText(canvas, font, 0, traincharspacing, color, textstring)
-                        graphics.DrawText(canvas, font, 53, traincharspacing, color, str(train[1]))
-                        traincharspacing+=8
-                        b+=1
-            if c<(len(self.packet)):
+            pos = 14
+            posi2=14
+            northhvalues=[[0, 30],[0,30]]
+            southhvalues=[[0, 30],[0,30]]
+            t_end = time.time() + 1 * 8  #last values controls display time per station in seconds
+            while time.time() < t_end:
+                canvas.Clear()
+                traincharspacing=7
+                b=0
+                for train in subpacket:
+                    if train[2][3] == "N":
+                        if b<2:
+                            color=self.getcolor(train[0])
+                            try:
+                                textstring=str(train[0])
+                            except:
+                                textstring="textfail"
+                            len2=(len(str(stops[train[2]]))*5)
+                            if len2>40:
+                                northhvalues[b][0]=10-len2+42
+                                posi2=northhvalues[b][1]
+                                if northhvalues[b][1]<=northhvalues[b][0]-20:
+                                    northhvalues[b][1]=30
+                                    posi2=northhvalues[b][1] 
+                                if posi2<=northhvalues[b][0] and posi2>=northhvalues[b][0]-20:
+                                    posi2=northhvalues[b][0]
+                                if northhvalues[b][1]>=10:
+                                    posi2=10
+                            else:
+                                posi2=10
+                            len3 = graphics.DrawText(canvas, font, posi2, traincharspacing, color, str(stops[train[2]]))
+                            for i in range(9):
+                                    graphics.DrawLine(canvas, i, traincharspacing-7, i, traincharspacing, graphics.Color(0, 0, 0))
+                            for i in range(52,64):
+                                    graphics.DrawLine(canvas, i, traincharspacing-7, i, traincharspacing, graphics.Color(0, 0, 0))
+                            graphics.DrawText(canvas, font, 0, traincharspacing, color, textstring)
+                            graphics.DrawText(canvas, font, 55, traincharspacing, color, str(train[1]))
+                            traincharspacing+=8
+                            northhvalues[b][1]-=1
+                            b+=1
+                traincharspacing=23
+                b=0
+                destinations=[]
+                for train in subpacket:
+                    if train[2][3] == "S":
+                        if b<2:
+                            color=self.getcolor(train[0])
+                            try:
+                                textstring=str(train[0])
+                            except:
+                                textstring="textfail"
+                            len2=(len(str(stops[train[2]]))*5)
+                            if len2>40:
+                                southhvalues[b][0]=10-len2+42
+                                posi2=southhvalues[b][1]
+                                if southhvalues[b][1]<=southhvalues[b][0]-20:
+                                    southhvalues[b][1]=30
+                                    posi2=southhvalues[b][1] 
+                                if posi2<=southhvalues[b][0] and posi2>=southhvalues[b][0]-20:
+                                    posi2=southhvalues[b][0]
+                                if southhvalues[b][1]>=10:
+                                    posi2=10
+                            else:
+                                posi2=10
+                            len3 = graphics.DrawText(canvas, font, posi2, traincharspacing, color, str(stops[train[2]]))
+                            for i in range(9):
+                                    graphics.DrawLine(canvas, i, traincharspacing-7, i, traincharspacing, graphics.Color(0, 0, 0))
+                            for i in range(52,64):
+                                    graphics.DrawLine(canvas, i, traincharspacing-7, i, traincharspacing, graphics.Color(0, 0, 0))
+                            graphics.DrawText(canvas, font, 0, traincharspacing, color, textstring)
+                            graphics.DrawText(canvas, font, 55, traincharspacing, color, str(train[1]))
+                            traincharspacing+=8
+                            southhvalues[b][1]-=1
+                            b+=1
+                time.sleep(0.05)
+                canvas = self.matrix.SwapOnVSync(canvas)
+            if c<3:
                 print("sleeping")
-                time.sleep(10)
+            elif c==3:
+                canvas.Clear()
+                graphics.DrawText(canvas, font, 0, 8, white, "Disruptions:")
+                print("servicedata showing")
+                charspace=0
+                vertspace=17
+                trainnum=1
+                for problemtrain in self.servicedata:
+                    color=self.getcolor(problemtrain)
+                    graphics.DrawText(canvas, font, charspace, vertspace, color, problemtrain)
+                    charspace +=7
+                    trainnum+=1
+                    if trainnum >= 10:
+                        vertspace+=8
+                        trainnum=1
+                        charspace=0
+                    #Add ip to disruptions screen
+                    ipaddr=str(check_output(['hostname', '-I']))[9:]
+                    
+                    ipaddr2="IP: " + ipaddr[:-3]
+                    graphics.DrawText(canvas, font, 0, 32, graphics.Color(10,169,172), ipaddr2)
+                canvas = self.matrix.SwapOnVSync(canvas)
             else:
-                print("not sleeping")      
-        
+                print("fetching data")      
 
-# Main function
 while True:
     if __name__ == "__main__":
-        packet=mtacalls.totalstationtimes(["232", "A41", "423"])
-        graphics_test = GraphicsTest(packet)
+        worked=0
+        while worked==0:
+            try:
+                packet=mtacalls2.totalstationtimes(["423", "A41", "232"])
+                servicedata=mtacalls2.procservicedata()
+                worked=1
+            except:
+                print("rebooting in 30 seconds")
+                time.sleep(30)
+                subprocess.Popen('sudo reboot -n', shell=True)              
+        for timegroup in packet:
+            for singletime in timegroup:
+                if singletime[1]<2:
+                    timegroup.remove(singletime)
+        
+        graphics_test = GraphicsTest(packet, servicedata)
+
         if (not graphics_test.process()):
             print("isrunning")
             graphics_test.print_help()
+            
